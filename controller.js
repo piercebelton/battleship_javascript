40985edd9d987d503760002a90fd03381af5d6f6
steph_max <stephandmax@learnacademy.org>
2016-11-23 11:20:33 -0800

    Story 2: Added click functionality to TDs to change color

--- controller.js
+++ controller.js
@@ -1,20 +1,28 @@
 $(document).ready(function() {
+  createBoard();
+
 //alert to be sure everything is linked properly
 //call for loops from the model to create the table!
+  $("td").on("click", function() {
+    $(this).addClass("clicked");
+  });
+
   $("#start").on("click", function() {
   buildTable();
-   //build a for loop within a for loop to create a 10x10 table. The first for loop creates the rows, the second creates the columns (aka the tds)
-    for (var i = 0; i < 10; i++) {
-      $("#table").append("<tr>");
-      for (var j = 0; j < 10; j++) {
-        $("#table").append("<td id=" + i + j + "></td>");
-      }
-      $("#table").append("</tr>");
-    }
+
   });
 
 
-
+   //build a for loop within a for loop to create a 10x10 table. The first for loop creates the rows, the second creates the columns (aka the tds)
+  function createBoard() {
+  for (var i = 0; i < 10; i++) {
+    $("#table").append("<tr id=" + i +">");
+    for (var j = 0; j < 10; j++) {
+      $("#"+i).append("<td id=" + i + j + "></td>");
+    }
+    $("#table").append("</tr>");
+    }
+  }
 
 
 
